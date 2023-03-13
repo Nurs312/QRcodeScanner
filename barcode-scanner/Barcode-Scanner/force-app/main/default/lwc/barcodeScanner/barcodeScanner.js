e1c8b7849977ce7b5d451ec2ae15403d069a9eec
Nurs312 <nursultan22.11.02@gmail.com>
2023-03-12 19:29:47 -0500

    Added feature to set package's status to Delivered

--- barcode-scanner/Barcode-Scanner/force-app/main/default/lwc/barcodeScanner/barcodeScanner.js
+++ barcode-scanner/Barcode-Scanner/force-app/main/default/lwc/barcodeScanner/barcodeScanner.js
@@ -2,15 +2,23 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getBarcodeScanner } from 'lightning/mobileCapabilities';
 import insertPackageRecord from '@salesforce/apex/barcodeScannerController.insertPackageRecord';
+import setStatusToDelivered from '@salesforce/apex/barcodeScannerController.setStatusToDelivered';
 
 export default class BarcodeScanner extends LightningElement {
     myScanner;
     scanButtonDisabled = false;
     scannedBarcode = '';
+    deliveredPackage = '';
     
 
     sendBarcodeValues(){
         insertPackageRecord({payload: JSON.stringify(this.scannedBarcode)})
+        .then((result)=>console.log(result))
+        .catch((error)=>console.log(error))
+    }
+
+    sendBarcodeValuesForUpdate(){
+        setStatusToDelivered({payload: JSON.stringify(this.deliveredPackage)})
         .then((result)=>console.log(result))
         .catch((error)=>console.log(error))
     }
@@ -20,6 +28,97 @@ export default class BarcodeScanner extends LightningElement {
         this.myScanner = getBarcodeScanner();
         if (this.myScanner == null || !this.myScanner.isAvailable()) {
             this.scanButtonDisabled = true;
+        }
+    }
+
+    // Handler for scanning     qr code to set package's status to "Delivered"
+    handleMarkAsDelivered(event) {
+        // Reset deliveredPackage to empty string before starting new scan
+        this.deliveredPackage = '';
+
+        // Make sure BarcodeScanner is available before trying to use it
+        // Note: We _also_ disable the Scan button if there's no BarcodeScanner
+        if (this.myScanner != null && this.myScanner.isAvailable()) {
+            const scanningOptions = {
+                barcodeTypes: [this.myScanner.barcodeTypes.QR],
+                instructionText: 'Scan a QR Code',
+                successText: 'Scanning complete.'
+            };
+            this.myScanner
+                .beginCapture(scanningOptions)
+                .then((result) => {
+                    console.log(result);
+                    // Do something with the barcode scan value:
+                    // - look up a record
+                    // - create or update a record
+                    // - parse data and put values into a form
+                    // - and so on; this is YOUR code
+                    // Here, we just display the scanned value in the UI
+                    this.deliveredPackage = JSON.parse(result.value);
+                    console.log(this.deliveredPackage);
+                    this.sendBarcodeValuesForUpdate();
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Successful Scan',
+                            message: 'Barcode scanned successfully.',
+                            variant: 'success'
+                        })
+                    );
+                })
+                .catch((error) => {
+                    // Handle cancellation and unexpected errors here
+                    console.error(error);
+
+                    if (error.code == 'userDismissedScanner') {
+                        // User clicked Cancel
+                        this.dispatchEvent(
+                            new ShowToastEvent({
+                                title: 'Scanning Cancelled',
+                                message:
+                                    'You cancelled the scanning session.',
+                                mode: 'sticky'
+                            })
+                        );
+                    }
+                    else { 
+                        // Inform the user we ran into something unexpected
+                        this.dispatchEvent(
+                            new ShowToastEvent({
+                                title: 'Barcode Scanner Error',
+                                message:
+                                    'There was a problem scanning the barcode: ' +
+                                    error.message,
+                                variant: 'error',
+                                mode: 'sticky'
+                            })
+                        );
+                    }
+                })
+                .finally(() => {
+                    console.log('#finally');
+
+                    // Clean up by ending capture,
+                    // whether we completed successfully or had an error
+                    this.myScanner.endCapture();
+                });
+        } else {
+            // BarcodeScanner is not available
+            // Not running on hardware with a camera, or some other context issue
+            console.log(
+                'Scan Barcode button should be disabled and unclickable.'
+            );
+            console.log('Somehow it got clicked: ');
+            console.log(event);
+
+            // Let user know they need to use a mobile phone with a camera
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Barcode Scanner Is Not Available',
+                    message:
+                        'Try again from the Salesforce app on a mobile device.',
+                    variant: 'error'
+                })
+            );
         }
     }
 
